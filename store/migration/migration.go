82c07c896a39948d3ea32a43b169edd935974042
Thomas Boerger <thomas@webhippie.de>
2016-11-15 09:52:31 +0100

    Embed postgres migrations

--- store/migration/migration.go
+++ store/migration/migration.go
@@ -1,6 +1,6 @@
 package migration
 
-//go:generate go-bindata -pkg migration -o bindata.go sqlite3/ mysql/
+//go:generate go-bindata -pkg migration -o bindata.go sqlite3/ mysql/ postgres/
 //go:generate go fmt bindata.go
 //go:generate sed -i.bak "s/Sql/SQL/" bindata.go
 //go:generate rm bindata.go.bak
